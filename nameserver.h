823e63925b5cb7a4a24ba80e93c0cb754fc4d54f
Haseeb Saeed <hs17@hotmail.co.uk>
2015-11-14 20:15:00 -0500

    Initial .h files

---
+++ nameserver.h
@@ -0,0 +1,15 @@
+#ifndef __NAMESERVER_H__
+#define __NAMESERVER_H__
+
+_Task NameServer {
+
+    void main();
+
+  public:
+    NameServer( Printer &prt, unsigned int numVendingMachines, unsigned int numStudents );
+    void VMregister( VendingMachine *vendingmachine );
+    VendingMachine *getMachine( unsigned int id );
+    VendingMachine **getMachineList();
+};
+
+#endif
